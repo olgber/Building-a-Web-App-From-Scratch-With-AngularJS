2da8e68f72275490001a6b49ae4923b78b3f7d2a
Marley <no_mames@mail.ru>
2014-10-17 03:31:38 +0100

    3.1 completed. ALL FINE

--- public/src/controllers.js
+++ public/src/controllers.js
@@ -1,5 +1,6 @@
 angular.module('ContactsApp')
-	.controller('ListController', function($scope, Contact, $location){
+	.controller('ListController', function($scope, $rootScope, Contact, $location){
+		$rootScope.PAGE = "all";
 		$scope.contacts = Contact.query();
 		$scope.fields = ['firstName', 'lastName'];
 
@@ -18,7 +19,8 @@ angular.module('ContactsApp')
 
 	})
 
-	.controller('NewController', function($scope, Contact, $location){
+	.controller('NewController', function($scope, $rootScope, Contact, $location){
+		$rootScope.PAGE = "new";
 		$scope.contact = new Contact({
 			firstName: ['', 'text'],
 			lastName: ['', 'text'],
@@ -40,12 +42,12 @@ angular.module('ContactsApp')
 		};
 
 	})
-	.controller('SingleController', function($scope, $location, Contact, $routeParams) {
+	.controller('SingleController', function($scope, $rootScope, $location, Contact, $routeParams) {
+		$rootScope.PAGE = "single";
 		$scope.contact = Contact.get({ id: parseInt($routeParams.id, 10) });
 		$scope.delete = function(){
 			$scope.contact.$delete();
 			$location.url('/contacts');
 		};
 	});
-
 

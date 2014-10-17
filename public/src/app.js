c4bcd29fe3a0eb00384c6bde133daaf357d020da
Marley <no_mames@mail.ru>
2014-10-17 23:55:32 +0100

    3.3 completed. Something not Working

--- public/src/app.js
+++ public/src/app.js
@@ -1,27 +1,34 @@
 angular.module('ContactsApp', ['ngRoute', 'ngResource', 'ngMessages'])
-	.config(function ($routeProvider, $locationProvider){
-		$routeProvider
-			.when('/contacts', {
-				controller: 'ListController',
-				templateUrl: 'views/list.html'
-			})
-			.when('/contact/new', {
-				controller: 'NewController',
-				templateUrl: 'views/new.html'
-			})
-			.when('/contact/:id',{
-				controller: 'SingleController',
-				templateUrl: 'views/single.html'
-			})
-			.otherwise({
-					redirectTo: '/contacts'
-				}
-			);
+    .config(function ($routeProvider, $locationProvider){
+        $routeProvider
+            .when('/contacts', {
+                controller: 'ListController',
+                templateUrl: 'views/list.html'
+            })
+            .when('/contact/new', {
+                controller: 'NewController',
+                templateUrl: 'views/new.html'
+            })
+            .when('/contact/:id',{
+                controller: 'SingleController',
+                templateUrl: 'views/single.html'
+            })
+            .when('/settings', {
+                controller: 'SettingsController',
+                templateUrl: 'views/settings.html'
+            })
+            .otherwise({
+                    redirectTo: '/contacts'
+                }
+            );
 
-		$locationProvider.html5Mode(true);
+        $locationProvider.html5Mode(true);
 
-	});
-
-
-
+    })
+    .value('options', {})
+    .run(function (options, Fields) {
+        Fields.get().success(function(data) {
+            options.displayed_fields = data;
+        });
+    });
 
